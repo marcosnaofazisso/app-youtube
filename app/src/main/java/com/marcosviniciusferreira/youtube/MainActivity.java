b12d88eacd37dd7e0b92dc94518099b9ae9e1ca8
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-20 16:54:41 -0300

    finishing environment settings, ready to work

--- app/src/main/java/com/marcosviniciusferreira/youtube/MainActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/youtube/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String GOOGLE_API_KEY = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
