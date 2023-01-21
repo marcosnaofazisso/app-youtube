39ab7fd23cdc2a5fa6bf2f2cc03395f0c1704857
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-21 18:43:44 -0300

    add channel id and base url

--- app/src/main/java/com/marcosviniciusferreira/youtube/helper/YoutubeConfig.java
+++ app/src/main/java/com/marcosviniciusferreira/youtube/helper/YoutubeConfig.java
@@ -2,7 +2,7 @@ package com.marcosviniciusferreira.youtube.helper;
 
 public class YoutubeConfig {
 
-    public static String CANAL_ID = "";
+    public static String CANAL_ID = "UCVHFbqXqoYvEWM1Ddxl0QDg";
     public static String CHAVE_YOUTUBE_API = "";
-    public static String BASE_URL = "";
+    public static String BASE_URL = "https://www.googleapis.com/youtube/v3/search";
 }
